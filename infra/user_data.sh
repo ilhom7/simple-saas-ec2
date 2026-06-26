85ba5c858427e0d2a8a434df8c321232778098ef
Ilhom Rahmanov <ilhom@Diors-MacBook-Air.local>
2026-06-26 13:54:11 -0400

    modified user_data.sh

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -4,10 +4,31 @@ set -eux
 dnf update -y
 dnf install -y docker git
 
-systemctl enable docker
-systemctl start docker
-
+systemctl enable --now docker
 usermod -aG docker ec2-user
 
+mkdir -p /usr/local/lib/docker/cli-plugins
+mkdir -p /usr/libexec/docker/cli-plugins
 mkdir -p /opt/simple-saas-ec2
+
+# Docker Compose
+curl -fSL \
+  https://github.com/docker/compose/releases/download/v2.39.4/docker-compose-linux-x86_64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-compose
+
+# Docker Buildx, required for: docker compose up --build
+curl -fSL \
+  https://github.com/docker/buildx/releases/download/v0.35.0/buildx-v0.35.0.linux-amd64 \
+  -o /usr/local/lib/docker/cli-plugins/docker-buildx
+
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
+
+cp /usr/local/lib/docker/cli-plugins/docker-compose /usr/libexec/docker/cli-plugins/docker-compose
+cp /usr/local/lib/docker/cli-plugins/docker-buildx /usr/libexec/docker/cli-plugins/docker-buildx
+
 chown -R ec2-user:ec2-user /opt/simple-saas-ec2
+
+docker --version
+docker compose version
+docker buildx version
