bae86e1f82257d723a65c08d3f7218ec9bdcc907
Ilhom Rahmanov <ilhom@Diors-MacBook-Air.local>
2026-06-26 22:19:25 -0400

    modified port in docker-compose.yml

--- infra/main.tf
+++ infra/main.tf
@@ -21,10 +21,41 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_subnet" "default_az1" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [data.aws_availability_zones.available.names[0]]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+data "aws_subnet" "default_az2" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [data.aws_availability_zones.available.names[1]]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
   }
 }
 
@@ -47,27 +78,48 @@ resource "aws_key_pair" "app" {
   key_name   = "${local.name}-key"
   public_key = file(var.public_key_path)
 }
-
-resource "aws_security_group" "app" {
-  name        = "${local.name}-sg"
-  description = "Allow HTTP and SSH"
+resource "aws_security_group" "alb" {
+  name        = "${local.name}-alb-sg"
+  description = "Allow HTTP traffic to ALB"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "HTTP access"
+    description = "HTTP from internet to ALB"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = "Allow outbound from ALB"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${local.name}-alb-sg"
+  }
 
+}
+resource "aws_security_group" "app" {
+  name        = "${local.name}-sg"
+  description = "Allow traffic from ALB and SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description     = "Allow ingress from ALB"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
   ingress {
     description = "SSH access for GitHub Actions deploy"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
   egress {
@@ -83,10 +135,58 @@ resource "aws_security_group" "app" {
   }
 }
 
+resource "aws_lb" "app" {
+  name               = "${local.name}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.alb.id]
+  subnets = [
+    data.aws_subnet.default_az1.id,
+    data.aws_subnet.default_az2.id
+  ]
+
+  tags = {
+    Name = "${local.name}-alb"
+  }
+}
+resource "aws_lb_target_group" "app" {
+  name        = "${local.name}-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    path                = "/health"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${local.name}-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
 resource "aws_instance" "app" {
   ami                         = data.aws_ami.amazon_linux_2023.id
   instance_type               = var.instance_type
-  subnet_id                   = data.aws_subnets.default.ids[0]
+  subnet_id                   = data.aws_subnet.default_az1.id
   vpc_security_group_ids      = [aws_security_group.app.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.app.key_name
@@ -96,4 +196,10 @@ resource "aws_instance" "app" {
   tags = {
     Name = "${local.name}-server"
   }
+}
+
+resource "aws_lb_target_group_attachment" "app" {
+  target_group_arn = aws_lb_target_group.app.arn
+  target_id        = aws_instance.app.id
+  port             = 3000
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,11 @@ output "ec2_public_ip" {
 output "app_url" {
   value = "http://${aws_instance.app.public_ip}"
 }
+
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
+}
+
+output "alb_url" {
+  value = "http://${aws_lb.app.dns_name}"
+}
